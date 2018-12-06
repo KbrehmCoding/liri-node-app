b0dbcbdfe95700889ab08ce36e4b58be93043707
Krista Brehm <kristabrehm1@gmail.com>
2018-12-06 00:02:09 -0500

    started coding the spotify api

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # liri-node-app
-spotify info
-Client ID 4840edb4a08348e0b4be664ed2591f8b
--- liri.js
+++ liri.js
@@ -1,1 +1,38 @@
-require("dotenv").config()
+require("dotenv").config();
+
+const axios = require('axios');
+const Spotify = require('node-spotify-api');
+
+const keys = require('./keys');
+
+const spotify = new Spotify(keys.spotify);
+
+spotify
+    .search({ type: 'track', query: 'All the Small Things' })
+    .then(response => {
+        // console.log('response', JSON.stringify(response));
+        spotify
+            .request(response.tracks.items[0].href)
+            .then(data => {
+                // console.log('data', JSON.stringify(data));
+                console.log('album name', data.album.name);
+                console.log('artist name', data.artists[0].name);
+                console.log('song name', data.name);
+                console.log('preview url', data.preview_url);
+            })
+            .catch(err => {
+                console.error('Error occurred: ' + err);
+            });
+    })
+    .catch(err => {
+        console.log('err', err);
+    });
+
+// spotify
+//     .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//     .then(function(data) {
+//         console.log('data', JSON.stringify(data));
+//     })
+//     .catch(function(err) {
+//         console.error('Error occurred: ' + err);
+//     });
